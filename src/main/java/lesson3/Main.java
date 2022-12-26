d8b77b7e6b7cdc995a7e854ae03c0c3003fb1251
sonicand <sonicandua@yahoo.com>
2022-12-26 17:49:38 +0200

    added lesson 3 file

---
+++ src/main/java/lesson3/Main.java
@@ -0,0 +1,8 @@
+package lesson3;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello git");
+        System.out.println("Push to gitgub");
+    }
+}
