097abe348c51207da90c13b39b30e6554e2e5d6e
sonicand <sonicandua@yahoo.com>
2023-01-01 14:10:39 +0200

    added lesson 3 file

--- src/main/java/lesson4/Homework4.java
+++ src/main/java/lesson4/Homework4.java
@@ -3,11 +3,11 @@ package lesson4;
 public class Homework4 {
     public static void main(String[] args) {
 
-        int length = 10;
-        int width = 20;
-        int height = 30;
-        int volume = length * width * height;
-        int sideLength = length*4;
+        int sideA = 10;
+        int sideB = 20;
+        int sideC = 30;
+        int volume = sideA * sideB * sideC;
+        int sideLength = (sideA + sideB + sideC) *4;
         System.out.println("Об'єм паралелепіпеда = " + volume);
         System.out.println("Суммарна довжина всіх сторін = " + sideLength);
     }
