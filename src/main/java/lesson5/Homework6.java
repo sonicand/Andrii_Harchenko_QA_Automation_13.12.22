db613c565b24b5ff5701a83f645419e2e06e7413
sonicand <sonicandua@yahoo.com>
2023-01-09 19:23:15 +0200

    some changes

--- src/main/java/lesson5/Homework6.java
+++ src/main/java/lesson5/Homework6.java
@@ -9,46 +9,46 @@ public class Homework6 {
         String FirstTeamName = scanner.nextLine();
         System.out.println("First team name is " + FirstTeamName);
         System.out.println("Please enter frag amount for first player of the " + FirstTeamName + " Team");
-        int T1P1FragCount = 0;
-        int T1P2FragCount = 0;
-        int T1P3FragCount = 0;
-        int T1P4FragCount = 0;
-        int T1P5FragCount = 0;
+        int t1P1FragCount = 0;
+        int t1P2FragCount = 0;
+        int t1P3FragCount = 0;
+        int t1P4FragCount = 0;
+        int t1P5FragCount = 0;
         if (scanner.hasNextInt()) {
-            T1P1FragCount = scanner.nextInt();
-            System.out.println(FirstTeamName + " Team first player frag count is " + T1P1FragCount);
+            t1P1FragCount = scanner.nextInt();
+            System.out.println(FirstTeamName + " Team first player frag count is " + t1P1FragCount);
         } else {
             System.out.println("Entered Wrong data!");
             System.exit(0);
         }
         System.out.println("Please enter frag amount for second player of the " + FirstTeamName + " Team");
         if (scanner.hasNextInt()) {
-            T1P2FragCount = scanner.nextInt();
-            System.out.println(FirstTeamName + " Team second player frag count is " + T1P2FragCount);
+            t1P2FragCount = scanner.nextInt();
+            System.out.println(FirstTeamName + " Team second player frag count is " + t1P2FragCount);
         } else {
             System.out.println("Entered Wrong data!");
             System.exit(0);
         }
         System.out.println("Please enter frag amount for second player of the " + FirstTeamName + " Team");
         if (scanner.hasNextInt()) {
-            T1P3FragCount = scanner.nextInt();
-            System.out.println(FirstTeamName + " Team third player frag count is " + T1P3FragCount);
+            t1P3FragCount = scanner.nextInt();
+            System.out.println(FirstTeamName + " Team third player frag count is " + t1P3FragCount);
         } else {
             System.out.println("Entered Wrong data!");
             System.exit(0);
         }
         System.out.println("Please enter frag amount for fourth player of the " + FirstTeamName + " Team");
         if (scanner.hasNextInt()) {
-            T1P4FragCount = scanner.nextInt();
-            System.out.println(FirstTeamName + " Team fourth player frag count is " + T1P4FragCount);
+            t1P4FragCount = scanner.nextInt();
+            System.out.println(FirstTeamName + " Team fourth player frag count is " + t1P4FragCount);
         } else {
             System.out.println("Entered Wrong data!");
             System.exit(0);
         }
         System.out.println("Please enter frag amount for fifth player of the " + FirstTeamName + " Team");
         if (scanner.hasNextInt()) {
-            T1P5FragCount = scanner.nextInt();
-            System.out.println(FirstTeamName + " Team fifth player frag count is " + T1P5FragCount);
+            t1P5FragCount = scanner.nextInt();
+            System.out.println(FirstTeamName + " Team fifth player frag count is " + t1P5FragCount);
         } else {
             System.out.println("Entered Wrong data!");
             System.exit(0);
@@ -57,55 +57,55 @@ public class Homework6 {
         System.out.println("Please enter second team name");
         String SecondTeamName = scanner.nextLine();
         System.out.println("Second team name is " + SecondTeamName);
-        int T2P1FragCount = 0;
-        int T2P2FragCount = 0;
-        int T2P3FragCount = 0;
-        int T2P4FragCount = 0;
-        int T2P5FragCount = 0;
+        int t2P1FragCount = 0;
+        int t2P2FragCount = 0;
+        int t2P3FragCount = 0;
+        int t2P4FragCount = 0;
+        int t2P5FragCount = 0;
         System.out.println("Please enter frag amount for first player of the " + SecondTeamName + " Team");
         if (scanner.hasNextInt()) {
-            T2P1FragCount = scanner.nextInt();
-            System.out.println(SecondTeamName + " Team first player frag count is " + T2P1FragCount);
+            t2P1FragCount = scanner.nextInt();
+            System.out.println(SecondTeamName + " Team first player frag count is " + t2P1FragCount);
         } else {
             System.out.println("Entered Wrong data!");
             System.exit(0);
         }
         System.out.println("Please enter frag amount for second player of the " + SecondTeamName + " Team");
         if (scanner.hasNextInt()) {
-            T2P2FragCount = scanner.nextInt();
-            System.out.println(SecondTeamName + " Team second player frag count is " + T2P2FragCount);
+            t2P2FragCount = scanner.nextInt();
+            System.out.println(SecondTeamName + " Team second player frag count is " + t2P2FragCount);
         } else {
             System.out.println("Entered Wrong data!");
             System.exit(0);
         }
         System.out.println("Please enter frag amount for second player of the " + SecondTeamName + " Team");
         if (scanner.hasNextInt()) {
-            T2P3FragCount = scanner.nextInt();
-            System.out.println(SecondTeamName + " Team third player frag count is " + T2P3FragCount);
+            t2P3FragCount = scanner.nextInt();
+            System.out.println(SecondTeamName + " Team third player frag count is " + t2P3FragCount);
         } else {
             System.out.println("Entered Wrong data!");
             System.exit(0);
         }
         System.out.println("Please enter frag amount for fourth player of the " + SecondTeamName + " Team");
         if (scanner.hasNextInt()) {
-            T2P4FragCount = scanner.nextInt();
-            System.out.println(SecondTeamName + " Team fourth player frag count is " + T2P4FragCount);
+            t2P4FragCount = scanner.nextInt();
+            System.out.println(SecondTeamName + " Team fourth player frag count is " + t2P4FragCount);
         } else {
             System.out.println("Entered Wrong data!");
             System.exit(0);
         }
         System.out.println("Please enter frag amount for fifth player of the " + SecondTeamName + " Team");
         if (scanner.hasNextInt()) {
-            T2P5FragCount = scanner.nextInt();
-            System.out.println(SecondTeamName + " Team fifth player frag count is " + T2P5FragCount);
+            t2P5FragCount = scanner.nextInt();
+            System.out.println(SecondTeamName + " Team fifth player frag count is " + t2P5FragCount);
         } else {
             System.out.println("Entered Wrong data!");
             System.exit(0);
         }
-        double AvgFirstTeamScore = (double)(T1P1FragCount + T1P2FragCount + T1P3FragCount + T1P4FragCount + T1P5FragCount) / 5;
-        double AvgSecondTeamScore = (double)(T2P1FragCount + T2P2FragCount + T2P3FragCount + T2P4FragCount + T2P5FragCount) / 5;
-        int FirstTeamScore = T1P1FragCount + T1P2FragCount + T1P3FragCount + T1P4FragCount + T1P5FragCount;
-        int SecondTeamScore = T2P1FragCount + T2P2FragCount + T2P3FragCount + T2P4FragCount + T2P5FragCount;
+        double AvgFirstTeamScore = (double)(t1P1FragCount + t1P2FragCount + t1P3FragCount + t1P4FragCount + t1P5FragCount) / 5;
+        double AvgSecondTeamScore = (double)(t2P1FragCount + t2P2FragCount + t2P3FragCount + t2P4FragCount + t2P5FragCount) / 5;
+        int FirstTeamScore = t1P1FragCount + t1P2FragCount + t1P3FragCount + t1P4FragCount + t1P5FragCount;
+        int SecondTeamScore = t2P1FragCount + t2P2FragCount + t2P3FragCount + t2P4FragCount + t2P5FragCount;
         String winner;
         int winnerScore;
         if (FirstTeamScore > SecondTeamScore) {
