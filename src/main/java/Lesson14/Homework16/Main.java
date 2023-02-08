cc57b32db49f1017138e3ce12b1ee7e938859a9e
sonicand <sonicandua@yahoo.com>
2023-02-08 10:24:36 +0200

    16 homework fix

--- src/main/java/Lesson14/Homework16/Main.java
+++ src/main/java/Lesson14/Homework16/Main.java
@@ -9,9 +9,9 @@ public class Main {
         AmdRyzen amdRyzen = new AmdRyzen(4000, "Ryzen 5 5800h", "AMD", "APU", 45, CpuTypeEnum.X86);
         System.out.println(amdRyzen.toString());
         AppleM1 appleM1 = new AppleM1(3500, "M1 Max", "Apple", "APU", "Integrated", CpuTypeEnum.ARM);
-        //System.out.println(AppleM1.toString());
+        System.out.println(appleM1.toString());
         IntelCore intelCore = new IntelCore(5000, "Core i5", "Intel", "Desktop", CpuTypeEnum.X64);
-        //System.out.println(IntelCore.toString());
+        System.out.println(intelCore.toString());
 
     }
 }
